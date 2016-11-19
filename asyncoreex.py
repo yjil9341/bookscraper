a57ec1609c7d66f61c2339006bed473e4751222e
Kim <sk004@eagles.bridgewater.edu>
2016-11-19 17:51:21 -0500

    second commit

--- asyncoreex.py
+++ asyncoreex.py
@@ -13,6 +13,7 @@ from bs4 import BeautifulSoup
 
 import grequests
 import time
+from datetime import datetime
 
 
 headers = {
@@ -22,28 +23,58 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
 }
 
-def fetch(url):
-    html = requests.get(url , headers = headers ).content
-    print url
-    return None
-##    soup = BeautifulSoup(html,'html.parser')
-##    print "Soup Found"
-##    return soup
+def log(msg):
+    # global logging function
+    if settings.log_stdout:
+        try:
+            print "{}: {}".format(datetime.now(), msg)
+        except UnicodeEncodeError:
+            pass  # squash logging errors in case of non-ascii text
 
+def saved_proxy_paid():
+    f = open('proxylist.csv','r')
+    text = f.readlines()
+    freshproxlist = []
+    for line in text:
+        prox ={
+            'http':'http://sk004:cpUGUJ4L@{}:{}/'.format(line.strip(),60099),
+            'https':'https://sk004:cpUGUJ4L@{}:{}/'.format(line.strip(),60099)
+            }
+        freshproxlist.append( prox )
+    f.close()
+    return freshproxlist
+
+def fetch(r, *args, **kwargs):
+    print (r.status_code,len(r.content))
+    
 def print_url(r, *args, **kwargs):
     print(r.url)
 
-if __name__ == '__main__':
     
-    urls = [
-        'http://www.heroku.com',
-        'http://www.tablib.org',
-        'http://www.httpbin.org',
-        'http://www.python-requests.org',
-        'http://www.kennethreitz.com'
-    ]
 
-    urls = urls*100
+if __name__ == '__main__':
+
+
+    f = open('isbns.txt','r')
+    lines = f.readlines()
+    isbns = []
+    log("Reading URLS ... ")
+    for line in lines:
+        isbn = line.strip()
+        isbns.append(isbn)
+    f.close()
+
+    ##Divides isbns into chunks
+    divider = lambda lst, sz: [lst[i:i+sz] for i in range(0, len(lst), sz)]
+    isbnschunklist = divider(isbns,10)
+    urls = []
+    for isbnchunk in isbnschunklist:
+        isbnchunk = "%7C".join(isbnchunk)
+        url = "https://www.amazon.com/gp/search/ref=sr_adv_b/?search-alias=stripbooks&unfiltered=1&field-isbn=" + isbnchunk + "&field-dateop=During&sort=relevanceexprank"
+        urls.append(url)
+
+    urls = urls[0:500]
+    
 
 ##    start = time.time()
 ##    p = Pool(8)
@@ -54,16 +85,12 @@ if __name__ == '__main__':
 
 
     start = time.time()
-    rs = (grequests.get(url , headers = headers , timeout = (1,5) , hooks = dict(response=print_url)) for url in urls)
+    proxs = saved_proxy_paid()
+    rs = (grequests.get(url , headers = headers , proxies = random.choice(proxs), timeout = (1,5) , hooks = dict(response=fetch)) for url in urls)
     ##Send the stack
     responses = grequests.map(rs, size = 50)
     print responses
     count = 0
-##    for response in responses:
-##        html = response.content
-##        soup = BeautifulSoup(html,'html.parser')
-##        print "Soup Found"
-##        response.close
-##        count += 1
+
     print count
     print "Running time: {}s".format(time.time()-start)
